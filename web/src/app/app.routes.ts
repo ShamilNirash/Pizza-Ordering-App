b2e2d6f646705866824be0a031ea18a7fa4f99fe
shamil nirash <shamilnirash@gmail.com>
2024-05-29 19:56:58 +0530

    feat: :sparkles: create userInfo component and integrate with backend

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { SigninComponent } from './pages/signin/signin.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [{ path: '', component: HomeComponent },
 {path:'sign-in', component:SigninComponent},
-{path:'sign-up',component:SignupComponent}
+{path:'sign-up',component:SignupComponent},
+{path:'profile', component:ProfileComponent}
 ];
