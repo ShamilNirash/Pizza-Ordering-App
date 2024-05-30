78c14b56aea3bba0cd3e9cacab7cb3f4dc8b3be8
shamil nirash <shamilnirash@gmail.com>
2024-05-30 23:29:08 +0530

    feat: :sparkles: complete profile component

--- web/src/app/component/profile/security/security.component.ts
+++ web/src/app/component/profile/security/security.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { UserAuthService } from '../../../services/user-auth/user-auth.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-security',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './security.component.html',
   styleUrl: './security.component.scss'
 })
 export class SecurityComponent {
-
+nextIcon= faAngleRight;
+constructor(private userAuthService:UserAuthService,private router:Router){}
+onClickLogOut(){
+  this.userAuthService.deleteCredentials();
+  this.router.navigateByUrl('/');
+}
 }
