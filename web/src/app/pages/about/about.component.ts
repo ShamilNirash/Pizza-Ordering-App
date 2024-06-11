d68ec868ad768923a8e1221f707c2a0128fed044
shamil nirash <shamilnirash@gmail.com>
2024-06-11 23:50:42 +0530

    feat: :sparkles: create about page

--- web/src/app/pages/about/about.component.ts
+++ web/src/app/pages/about/about.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { AboutCoverPhotoComponent } from '../../component/about/about-cover-photo/about-cover-photo.component';
+import { AboutDescriptionComponent } from '../../component/about/about-description/about-description.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,AboutCoverPhotoComponent,AboutDescriptionComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
