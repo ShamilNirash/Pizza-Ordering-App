0aa552c58d8be4fd17fb752ca9bd58f592fc98d9
shamil nirash <shamilnirash@gmail.com>
2024-07-01 17:28:00 +0530

    feat: :sparkles: create loading component

---
+++ web/src/app/interceptors/loading.interceptor.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpEventType,
+} from '@angular/common/http';
+import { LoadingService } from '../services/loading/loading.service';
+import { Observable, tap } from 'rxjs';
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+  constructor(private loadingService: LoadingService) {}
+  pendingRequests = 0;
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    this.loadingService.showLoading();
+    this.pendingRequests = this.pendingRequests + 1;
+    return next.handle(request).pipe(
+      tap({
+        next: res => {
+          console.log(res.type);
+          if (res.type === HttpEventType.Response) {
+            this.hideLoading();
+          }
+        },
+        error: err => {
+          this.hideLoading();
+        },
+      })
+    );
+  }
+
+  hideLoading() {
+    this.pendingRequests -= 1;
+    if (this.pendingRequests == 0) {
+      this.loadingService.hideLoading();
+    }
+  }
+}
